09d103e303729fe20808ff58affe9bdb13715116
Sbonelo01 <sbonelomkhize15@gmail.com>
2025-08-08 00:16:23 +0200

    latest podcast episode

--- src/components/PodcastEmbed.tsx
+++ src/components/PodcastEmbed.tsx
@@ -13,7 +13,7 @@ const PodcastEmbed = () => {
         
         <div className="bg-card rounded-3xl overflow-hidden shadow-card p-8">
           <div className="aspect-video bg-gradient-primary rounded-2xl flex items-center justify-center">
-            <div className="text-center text-primary-foreground">
+            {/* <div className="text-center text-primary-foreground">
               <div className="w-16 h-16 mx-auto mb-4 rounded-full bg-white/20 flex items-center justify-center">
                 <svg
                   className="w-8 h-8"
@@ -27,7 +27,8 @@ const PodcastEmbed = () => {
               <p className="text-sm opacity-90">
                 Click to listen on your favorite platform
               </p>
-            </div>
+            </div> */}
+            <iframe width="738" height="415" src="https://www.youtube.com/embed/3xvKS9L5Xbs?list=LL" title="Episode 7 - Part 1 | The Fundamentals of Pet Nutrition with Romain – Pet Nutritionist &amp; Brand Owner" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>
         </div>
       </div>
